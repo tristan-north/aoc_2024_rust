1f44d32100c76e3288fbdc153409dfd09d094354
Tristan North <git@tristan-north.com>
2025-10-21 23:26:32 +1100

    Completed day1 part1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let (mut a, mut b): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .filter_map(|line| line.split_once("   "))
+        .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+        .unzip();
+
+    a.sort();
+    b.sort();
+
+    let accum: i32 = a.iter().zip(b).map(|(a, b)| (a - b).abs()).sum();
+
+    println!("{accum}");
+
+    Ok(())
 }
