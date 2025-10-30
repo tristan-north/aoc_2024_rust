b4d86ec5049be41d4ce7657af7d4a8e0b5a8abb7
Tristan North <git@tristan-north.com>
2025-10-30 19:50:03 +1100

    Day 3 Part Two solution.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,14 +1,34 @@
+#[derive(PartialEq, Debug)]
+enum ExecuteState {
+    Enabled,
+    Disabled,
+}
+
 fn main() {
-    let input = std::fs::read_to_string("input_example.txt").expect("Couldn't read input");
+    let input = std::fs::read_to_string("input.txt").expect("Couldn't read input");
     let input = input.trim_ascii_end();
 
     let mut accum = 0;
+    let mut execute_check_start_index = 0;
+    let mut execute_state = ExecuteState::Enabled;
 
     for (i, _) in input.match_indices("mul(") {
-        let start = i;
+        // Check for any do and don't commands betwen where we last checked
+        // up to the current mul match
+        let execute_check_slice = &input[execute_check_start_index..i];
+        if let Some(v) = update_execute_state(execute_check_slice) {
+            execute_state = v;
+            println!("Execute state changed to: {execute_state:?}");
+        }
+        if execute_state == ExecuteState::Disabled {
+            execute_check_start_index = i;
+            continue;
+        }
+
+        // End slice for easy printing of the relevent chars
         let end = std::cmp::min(i + 12, input.len());
 
-        let substr = &input[start..end];
+        let substr = &input[i..end];
 
         // Check first number
         let substr = &substr[4..];
@@ -16,12 +36,16 @@ fn main() {
 
         let first_num = match get_next_number(substr) {
             Some(v) => v,
-            _ => continue,
+            _ => {
+                execute_check_start_index = i;
+                continue;
+            }
         };
         // substr now starts at ,
         let substr = &substr[first_num.len()..];
 
         if !is_next_char(substr, ',') {
+            execute_check_start_index = i;
             continue;
         }
 
@@ -29,18 +53,23 @@ fn main() {
         let substr = &substr[1..];
         let second_num = match get_next_number(substr) {
             Some(v) => v,
-            _ => continue,
+            _ => {
+                execute_check_start_index = i;
+                continue;
+            }
         };
         // substr now starts at )
         let substr = &substr[second_num.len()..];
 
         if !is_next_char(substr, ')') {
+            execute_check_start_index = i;
             continue;
         }
 
         println!("Found {first_num} * {second_num}");
 
         accum += first_num.parse::<u32>().unwrap() * second_num.parse::<u32>().unwrap();
+        execute_check_start_index = i;
     }
 
     println!("\nPart One Solution: {accum}");
@@ -65,5 +94,33 @@ fn get_next_number(text: &str) -> Option<String> {
         return None;
     } else {
         return Some(first_num);
+    }
+}
+
+fn update_execute_state(text: &str) -> Option<ExecuteState> {
+    let do_index = match text.rmatch_indices("do()").next() {
+        Some(v) => {
+            let (i, _) = v;
+            i as i32
+        }
+        _ => -1,
+    };
+
+    let dont_index = match text.rmatch_indices("don't()").next() {
+        Some(v) => {
+            let (i, _) = v;
+            i as i32
+        }
+        _ => -1,
+    };
+
+    if do_index == -1 && dont_index == -1 {
+        return None;
+    }
+
+    if do_index > dont_index {
+        return Some(ExecuteState::Enabled);
+    } else {
+        return Some(ExecuteState::Disabled);
     }
 }
